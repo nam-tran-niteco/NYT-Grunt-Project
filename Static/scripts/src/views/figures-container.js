25f783f096ef1dcaf4fea9454ba3f1bd5a188773
Tran Hoai Nam <namth27494@gmail.com>
2016-08-10 23:50:06 +0700

    add waypoint, number ticker to figures-container, Complete 3 parts

--- Static/scripts/src/views/figures-container.js
+++ Static/scripts/src/views/figures-container.js
@@ -5,20 +5,38 @@
         var time_stop = 5000
         $('.figures-container').waypoint({
             handler: function() {
-                for(let i = 0; i < 4; i++){
-                    var item = $('.item-' + i)
-                    var count = item.attr('count')
-                    var loop_number = 0
-                    var offset = count / time_stop
-                    setInterval(function(){
-                        if(loop_number > count){
-                            item.html(count)
-                            return
+                // for(let i = 0; i < 4; i++){
+                //     var item = $('.item-' + i)
+                //     var count = item.attr('count')
+                //     var loop_number = 0
+                //     var offset = count / time_stop
+                //     setInterval(function(){
+                //         if(loop_number > count){
+                //             item.html(count)
+                //             return
+                //         }
+                //         loop_number += offset 
+                //         item.html(Math.round(loop_number))
+                //     }, time_stop / count)
+                // }
+                $('.figure__item i').each(function(){
+                    $(this).prop('Counter', 0).animate({
+                        Counter: $(this).parent().attr('count')
+                    }, {
+                        duration: 4000,
+                        easing: 'swing',
+                        step: function(now){
+                            now = Math.ceil(now)
+                            if(now >= 1000){
+                                var suffix = now % 1000
+                                if(suffix < 10) suffix = '00' + suffix
+                                else if(suffix < 100) suffix = '0' + suffix   
+                                now = Math.ceil(now / 1000) + ',' + suffix
+                            }
+                            $(this).text(now)
                         }
-                        loop_number += offset 
-                        item.html(Math.round(loop_number))
-                    }, time_stop / count)
-                }
+                    })
+                })
                 $(this).waypoint('destroy');
             },
             offset: "60%"
