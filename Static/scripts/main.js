79a9c417c71980f89c8c0baa5c98c7f324fe06a3
Trang Minh Nguyen <trang.nguyen4@niteco.se>
2016-08-15 18:10:48 +0700

    Done

--- Static/scripts/main.js
+++ Static/scripts/main.js
@@ -7,29 +7,3 @@ define(['jquery', 'underscore', 'base/modules/jitRequire', 'vendor/fastclick', '
 
         pageScrollAnimation.init($(document));
     });
-
-$(document).ready(function() {
-    $('.brand-logos').each(function() {
-        var $this = $(this);
-        $this.owlCarousel({
-            items: 1,
-            dots: false,
-            nav: true,
-            navText: [
-                '<span class="glyphicon glyphicon-chevron-left arrow-circle" aria-hidden="true"></span>',
-                '<span class="glyphicon glyphicon-chevron-right arrow-circle" aria-hidden="true"></span>'
-            ],
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 3
-                },
-                1000: {
-                    items: 5
-                }
-            }
-        });
-    });
-});
